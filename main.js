73d691b00388369a8a324caab9f19d2b7b282957
kurururu2004 <Kurmanbekovaarina20003@gmail.com>
2022-09-21 20:12:53 +0300

    dz3

---
+++ main.js
@@ -0,0 +1,29 @@
+// const svetofor = prompt ('Введите текст (green,red or yellow)')
+// switch(svetofor){
+//     case "green": 
+//         alert('Можно идти');
+//         break;
+//     case "red":
+//         alert('Стой');
+//         break;
+//     case "yellow":
+//         alert('Присмотрись');
+//         break;
+//         case "" :
+//             alert('введи текст')
+//             break;
+// }
+
+
+// let num1 = parseInt(prompt('введите число'))
+// let num2 = parseInt(prompt('введите 2 число'))
+
+// if (num1 > num2 ) {
+//     alert('num1 win')
+// }else if (num1 < num2) {
+//     alert('num2 win')
+// }else if(num1 === num2) {
+//     alert('draw')
+// }else if (isNaN(num1) && isNaN(num2) || isNaN(num1) || isNaN(num2)) {
+//     alert('введите число')
+// }
